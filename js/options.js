fcb2701697f55e230da4f28aae351319b230c760
Dmitry <dmitryvo83@gmail.com>
2020-04-16 18:56:36 +0300

    completed

--- js/options.js
+++ js/options.js
@@ -14,27 +14,32 @@ window.addEventListener('DOMContentLoaded', function () {
             var div = document.createElement('div');
             // div.textContent = prompt('Please type in your text');         
             div.textContent = a;
-           
-            let nameOfElemConstr = ['height', 'width', 'bg', 'fontSize', 'textAlign'];
-            let elemQntConstr = [this.height, this.width, this.bg, this.fontSize, this.textAlign];
+            document.body.appendChild(div);
 
-            let createCssStyleElem = [];
-            for (let i = 0; i < nameOfElemConstr.length, i < elemQntConstr.length; i++) {
-            // div.style.cssText = `${nameOfElemConstr[i]}: ${elemQntConstr[i]};`;   
-            createCssStyleElem = `${nameOfElemConstr[i]}: ${elemQntConstr[i]};`;
-                // div.style.cssText =  
-            }
+            let param = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontStyle}px; text-align: ${this.textAlign}`;
+            div.style.cssText = param;
+
+            // let nameOfElemConstr = ['height', 'width', 'bg', 'fontSize', 'textAlign'];
+            // let elemQntConstr = [this.height, this.width, this.bg, this.fontSize, this.textAlign];
+
+            // let createCssStyleElem;
+            // for (let i = 0; i < nameOfElemConstr.length, i < elemQntConstr.length; i++) {
+            // // div.style.cssText = `${nameOfElemConstr[i]}: ${elemQntConstr[i]};`;   
+            // createCssStyleElem = `${nameOfElemConstr[i]}: ${elemQntConstr[i]};`;
+            //     // div.style.cssText =  
+            // }
             
-            console.log(createCssStyleElem);
+            // console.log(createCssStyleElem);
+
+            // div.style.createCssStyleElem;
 
             // for (let i = 0; i < nameOfElemConstr.length, i < elemQntConstr.length; i++) {
             // div.setAttribute = `'style', 'nameOfElemConstr[i]: elemQntConstr[i];`;            
             // }
         }
-        
     }
 
-    let newDiv = new Options('45px', '100px', '#ccc', '14px', 'middle');      
+    const newDiv = new Options(200, 200, '#ccc', 54, 'center');      
     newDiv.createDiv('this is a new div with text');
 
 });
