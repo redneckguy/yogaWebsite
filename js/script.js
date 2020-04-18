2ddede952c339fc208c222c2894addba86262fab
Dmitry <dmitryvo83@gmail.com>
2020-04-18 22:47:41 +0300

    18/04/20 22.47 calculator completed

--- js/script.js
+++ js/script.js
@@ -235,23 +235,67 @@ window.addEventListener('DOMContentLoaded', function () {
     function plusSlides(n) {
         showSlides(slideIndex += n);
     }
+
     function currentSlide(n) {
         showSlides(slideIndex = n);
     }
 
-    prev.addEventListener('click', function() {
-        plusSlides(-1);        
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
     });
 
-    next.addEventListener('click', function() {
-        plusSlides(1);        
+    next.addEventListener('click', function () {
+        plusSlides(1);
     });
 
-    dotsWrap.addEventListener('click', function(event) {
+    dotsWrap.addEventListener('click', function (event) {
         for (let i = 0; i < dots.length + 1; i++) {
-            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
                 currentSlide(i);
             }
+        }
+    });
+
+    //CALC===============================================================================
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function () {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 2000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
         }
     });
 });
